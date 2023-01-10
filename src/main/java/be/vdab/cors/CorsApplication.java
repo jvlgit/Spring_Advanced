898925f182bd6588156addbabdaf7e0b1a6113a1
Jan Van Liefferinge <jan.vanliefferinge@vdabcampus.be>
2023-01-10 13:44:22 +0100

    postman

--- src/main/java/be/vdab/cors/CorsApplication.java
+++ src/main/java/be/vdab/cors/CorsApplication.java
@@ -1,7 +1,12 @@
 package be.vdab.cors;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.webmvc.api.OpenApiActuatorResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class CorsApplication {
@@ -10,4 +15,9 @@ public class CorsApplication {
         SpringApplication.run(CorsApplication.class, args);
     }
 
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("Rekenen").version("1.0.0")
+                .description("Berekeningen maken"));
+    }
 }
